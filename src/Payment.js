d715c1dbab91be85e19c671de27aafca7e90cd22
Yakov Kovalev <yataw@ya.ru>
2019-09-14 20:49:36 +0300

    translate map from raw html to react component

--- src/Payment.js
+++ src/Payment.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import {BrowserRouter as Router, Route, Link} from "react-router-dom";
 
-class Navigation extends Component {
+class Payment extends Component {
     render() {
         return (
             <nav className="navbar navbar-light navbar-expand-md" id="navbarbootom"
@@ -9,12 +9,12 @@ class Navigation extends Component {
                 <div className="container-fluid">
                     <Link to='/'><i className="far fa-money-bill-alt" style={{fontSize: '33px'}}></i></Link>
                     <Link to='/history'><i className="fas fa-history" style={{fontSize: '33px'}}></i></Link>
-                    <Link to='/map'><i className="fas fa-map-marked-alt" style={{fontSize: '33px'}}></i></Link>
-                    <Link to='/payment'><i className="fab fa-cc-apple-pay" style={{fontSize: '33px'}}></i></Link>
+                    <Link to='/'><i className="fas fa-map-marked-alt" style={{fontSize: '33px'}}></i></Link>
+                    <Link to='/'><i className="fab fa-cc-apple-pay" style={{fontSize: '33px'}}></i></Link>
                 </div>
             </nav>
         )
     }
 }
 
-export default Navigation;
+export default Payment;
